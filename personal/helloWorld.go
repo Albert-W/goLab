c2929cc3f9f0fca0af3f0bec652f822429b9a81b
Albert-W <wuyichang1990@gmail.com>
2018-09-05 09:00:02 +0100

    add

--- personal/helloWorld.go
+++ personal/helloWorld.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//"runtime"
+	"math/cmplx"
+)
+func variable(){
+	var a int =0
+	var s string ="abc"
+	fmt.Print(a,s)
+}
+
+func enums()  {
+	
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	//go func() {
+	//	fmt.Println("1")
+	//}()
+	//go func() {
+	//	fmt.Println("2")
+	//}()
+	//go func() {
+	//	fmt.Println("3")
+	//}()
+	//runtime.Gosched()
+	variable()
+	c := 3+4i
+	fmt.Println(cmplx.Abs(c))
+
+	
+
 }
